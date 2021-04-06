3bea94821bb7ed94844979f98a1b8fa44961b675
DnLegend <60458795+DnLegend@users.noreply.github.com>
2021-04-06 16:47:10 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -7,14 +7,14 @@ addLayer("green", {
 		points: new Decimal(0),
     }},
     color: "#4BDC13",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    requires: new Decimal(5), // Can be a function that takes requirement increases into account
     resource: "Creatures", // Name of prestige currency
     baseResource: "Green Mana", // Name of resource prestige is based on
     baseAmount() {
         return new Decimal(1)
     }, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    exponent: 2, // Prestige currency exponent
     upgrades: {
       rows: 2,
       cols: 5,
