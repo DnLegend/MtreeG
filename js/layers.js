61d1de2b5c774d29dccc6392e1031dfc3a6f8fbe
DnLegend <60458795+DnLegend@users.noreply.github.com>
2021-04-06 19:45:20 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -100,7 +100,7 @@ addLayer("empty"){
   row: 1,
   layerShown: false
 },
-addLayer("Blue", {
+addLayer("blue", {
     name: "Creatures", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "U", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 2, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
@@ -131,7 +131,7 @@ addLayer("Blue", {
     },
     layerShown(){return true}
 }),
-addLayer("Red", {
+addLayer("red", {
         name: "Creatures", // This is optional, only used in a few places, If absent it just uses the layer id.
         symbol: "R", // This appears on the layer's node. Default is the id with the first letter capitalized
         position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
