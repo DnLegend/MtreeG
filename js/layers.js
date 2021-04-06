ad3c3f574d744599260c8c51c8dcdb42f4ac0547
DnLegend <60458795+DnLegend@users.noreply.github.com>
2021-04-06 19:20:44 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,7 +1,39 @@
+addLayer("white", {
+    name: "Creatures", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "W", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    row: 0,
+    startData() { return {
+      unlocked: true,
+  	  points: new Decimal(0),
+    }},
+    color: "#4BDC13",
+    requires: new Decimal(5), // Can be a function that takes requirement increases into account
+    resource: "Creatures", // Name of prestige currency
+    baseResource: "White Mana", // Name of resource prestige is based on
+    baseAmount() {
+      return new Decimal(1)
+    }, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 2, // Prestige currency exponent
+    upgrades: {
+    rows: 2,
+    cols: 5,
+    11: {
+      title: "Play a Forest",
+      type: "Land",
+      description: "Generate 1 Green Mana per second",
+      cost: new Decimal(1),
+      unlocked() {return player.green.unlocked},
+    },
+  },
+  layerShown(){return true}
+}),
 addLayer("green", {
     name: "Creatures", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "G", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    row: 1,
     startData() { return {
       unlocked: true,
 		  points: new Decimal(0),
@@ -60,19 +92,24 @@ addLayer("green", {
     ],
     layerShown(){return true}
 }),
-addLayer("black", {
+addLayer("empty"){
+  position: 1,
+  row: 1,
+  layerShown: false
+},
+addLayer("Blue", {
     name: "Creatures", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "B", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    symbol: "U", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 2, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     row: 1,
     startData() { return {
         unlocked: true,
-		points: new Decimal(0),
+    points: new Decimal(0),
     }},
     color: "#4BDC13",
     requires: new Decimal(5), // Can be a function that takes requirement increases into account
     resource: "Creatures", // Name of prestige currency
-    baseResource: "Black Mana", // Name of resource prestige is based on
+    baseResource: "Blue Mana", // Name of resource prestige is based on
     baseAmount() {
         return new Decimal(1)
     }, // Get the current amount of baseResource
@@ -91,41 +128,10 @@ addLayer("black", {
     },
     layerShown(){return true}
 }),
-addLayer("white", {
-    name: "Creatures", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "W", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    row: 1,
-    startData() { return {
-      unlocked: true,
-  	  points: new Decimal(0),
-    }},
-    color: "#4BDC13",
-    requires: new Decimal(5), // Can be a function that takes requirement increases into account
-    resource: "Creatures", // Name of prestige currency
-    baseResource: "White Mana", // Name of resource prestige is based on
-    baseAmount() {
-      return new Decimal(1)
-    }, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 2, // Prestige currency exponent
-    upgrades: {
-    rows: 2,
-    cols: 5,
-    11: {
-      title: "Play a Forest",
-      type: "Land",
-      description: "Generate 1 Green Mana per second",
-      cost: new Decimal(1),
-      unlocked() {return player.green.unlocked},
-    },
-  },
-  layerShown(){return true}
-}),
-    addLayer("Red", {
+addLayer("Red", {
         name: "Creatures", // This is optional, only used in a few places, If absent it just uses the layer id.
         symbol: "R", // This appears on the layer's node. Default is the id with the first letter capitalized
-        position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+        position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
         row: 2,
         startData() { return {
             unlocked: true,
@@ -153,34 +159,34 @@ addLayer("white", {
   },
   layerShown(){return true}
 }),
-      addLayer("Blue", {
-          name: "Creatures", // This is optional, only used in a few places, If absent it just uses the layer id.
-          symbol: "U", // This appears on the layer's node. Default is the id with the first letter capitalized
-          position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-          row: 2,
-          startData() { return {
-              unlocked: true,
-      		points: new Decimal(0),
-          }},
-          color: "#4BDC13",
-          requires: new Decimal(5), // Can be a function that takes requirement increases into account
-          resource: "Creatures", // Name of prestige currency
-          baseResource: "Blue Mana", // Name of resource prestige is based on
-          baseAmount() {
-              return new Decimal(1)
-          }, // Get the current amount of baseResource
-          type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-          exponent: 2, // Prestige currency exponent
-          upgrades: {
-            rows: 2,
-            cols: 5,
-            11: {
-              title: "Play a Forest",
-              type: "Land",
-              description: "Generate 1 Green Mana per second",
-              cost: new Decimal(1),
-              unlocked() {return player.green.unlocked},
-            },
-          },
-          layerShown(){return true}
-        })
+addLayer("black", {
+    name: "Creatures", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "B", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    row: 2,
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#4BDC13",
+    requires: new Decimal(5), // Can be a function that takes requirement increases into account
+    resource: "Creatures", // Name of prestige currency
+    baseResource: "Black Mana", // Name of resource prestige is based on
+    baseAmount() {
+        return new Decimal(1)
+    }, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 2, // Prestige currency exponent
+    upgrades: {
+      rows: 2,
+      cols: 5,
+      11: {
+        title: "Play a Forest",
+        type: "Land",
+        description: "Generate 1 Green Mana per second",
+        cost: new Decimal(1),
+        unlocked() {return player.green.unlocked},
+      },
+    },
+    layerShown(){return true}
+})
